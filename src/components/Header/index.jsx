2339f4be3abc8868b223eb4135dcef821716ab85
Kanykeii <alymkulovakanykej3@gmail.com>
2022-09-06 18:27:15 +0600

    lasy

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import cls from './Header.module.scss'
+import { img } from './image/vhs.jpg'
+
+const Header = () => {
+  return (
+    <img src={img} alt="" />
+  )
+}
+
+export default Header
